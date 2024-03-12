680df007f58a048901db31b017b44c73b280a7da
espressowizard <hajimedevlife@gmail.com>
2024-03-12 13:50:46 +0700

    add orbit controls

--- js/main.js
+++ js/main.js
@@ -1,47 +1,52 @@
 import * as THREE from "three";
-import { getFOV } from "./helper";
 import GUI from "lil-gui";
 import vertexShader from "../shaders/vertex.glsl";
 import fragmentShader from "../shaders/fragment.glsl";
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const g_width = window.innerWidth;
 const g_height = window.innerHeight;
 const g_pixelRatio = window.devicePixelRatio;
 const g_ratio = g_width / g_height;
+const g_uniforms = {
+    u_time: { value: 0 }
+};
+const g_settings = {};
 
 class WebGL {
     perspective = 1000;
 
     constructor(canvas) {
         this.canvas = canvas;
+        this.uniforms = g_uniforms;
+        this.settings = g_settings;
+
         this.gui = new GUI();
-        this.uniforms = {
-            u_time: { value: 0 }
-        };
-        this.settings = {};
+        this.scene = new THREE.Scene();
     }
 
     setup() {
-        this.scene = new THREE.Scene();
-
-        const fov = getFOV(g_height, this.perspective);
-        this.camera = new THREE.PerspectiveCamera(fov, g_ratio, 0.1, this.perspective);
-        this.camera.position.set(0, 0, this.perspective);
+        this.camera = new THREE.PerspectiveCamera(75, g_ratio);
+        this.camera.position.set(0, 0, this.settings.z);
 
         this.renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
         this.renderer.setSize(g_width, g_height);
         this.renderer.setPixelRatio(g_pixelRatio);
-
         this.canvas.appendChild(this.renderer.domElement);
+
+        this.setupExtension();
+    }
+
+    setupExtension() {
+        this.control = new OrbitControls(this.camera, this.renderer.domElement);
     }
 
     createMesh() {
-        const sphereGeometry = new THREE.SphereGeometry(4, 100, 100);
+        const sphereGeometry = new THREE.PlaneGeometry(1, 1, 100, 100);
         const material = new THREE.ShaderMaterial({ 
             uniforms: this.uniforms,
             vertexShader,
             fragmentShader,
-            wireframe: true,
         });
 
         this.mesh = new THREE.Mesh(sphereGeometry, material);
